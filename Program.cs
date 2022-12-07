7ce4925fca51dc1163114ea2d11e987d4d215aa7
anushkachouhan029 <anushkachouhan029>
2022-12-07 22:31:42 +0530

    added num of 1st position

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Generics Program\n");
+            int output = MaxValue.MaxIntegerNumber(3333, 222, 11);
+            Console.WriteLine("Max integer number is: " + output);
+        }
+    }
+}

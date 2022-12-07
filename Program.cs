27c23d9ac77c4049b87c53dd0e8fd0a17152d9cf
anushkachouhan029 <anushkachouhan029>
2022-12-07 22:21:02 +0530

    added max integer

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Generics Program\n");
+            int output = MaxValue.MaxIntegerNumber(11, 222, 3333);
+            Console.WriteLine("Max integer number is: " + output);
+        }
+    }
+}

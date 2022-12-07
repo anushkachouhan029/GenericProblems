ba66bb81c1403dded25fd5f52290638579c58d84
anushkachouhan029 <anushkachouhan029>
2022-12-07 22:08:38 +0530

    added refactor2 project

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System;
+
+namespace GenericProblems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wellcome to Generics Program");
+
+            MaxValue<int> maxValueint = new MaxValue<int>(11, 222, 3333);
+            int output1 = maxValueint.MaxMethod();
+            Console.WriteLine("\nMax integer number is: " + output1);
+
+            MaxValue<float> maxValuefloat = new MaxValue<float>(1.1f, 2.22f, 3.333f);
+            float output2 = maxValuefloat.MaxMethod();
+            Console.WriteLine("\nMax float number is: " + output2);
+
+            MaxValue<string> maxValuestring = new MaxValue<string>("Apple", "Peach", "Banana");
+            string output3 = maxValuestring.MaxMethod();
+            Console.WriteLine("\nMax String is: " + output3);
+        }
+    }
+}
---
+++ maxvalue.cs
@@ -0,0 +1,34 @@
+﻿namespace GenericProblems
+{
+    internal class MaxValue<T> where T : IComparable
+    {
+        public T Value1, Value2, Value3;
+        public MaxValue(T value1, T value2, T value3)
+        {
+            this.Value1 = value1;
+            this.Value2 = value2;
+            this.Value3 = value3;
+        }
+        public static T MaxValueCheck(T value1, T value2, T value3)
+        {
+            if (value1.CompareTo(value2) > 0 && value1.CompareTo(value3) > 0)
+            {
+                return value1;
+            }
+            if (value2.CompareTo(value1) > 0 && value2.CompareTo(value3) > 0)
+            {
+                return value2;
+            }
+            if (value3.CompareTo(value1) > 0 && value3.CompareTo(value2) > 0)
+            {
+                return value3;
+            }
+            return default;
+        }
+        public T MaxMethod()
+        {
+            T max = MaxValue<T>.MaxValueCheck(this.Value1, this.Value2, this.Value3);
+            return max;
+        }
+    }
+}
